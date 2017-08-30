f73af905fbfbbe6254aed1f5cf12e59ef63b34a9
Joseph <jrichardson@jmstudios.net>
2017-08-30 14:55:16 -0700

    started small api and models for jokes and riddles

---
+++ Jester2.0/EnigmaModel.swift
@@ -0,0 +1,14 @@
+//
+//  EnigmaModel.swift
+//  Jester2.0
+//
+//  Created by Joseph Richardson on 8/30/17.
+//  Copyright © 2017 Joseph Richardson. All rights reserved.
+//
+
+import Foundation
+
+class Enigma {
+    var type: String = ""
+
+}
---
+++ Jester2.0/api.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  Jester2.0
+//
+//  Created by Joseph Richardson on 8/30/17.
+//  Copyright © 2017 Joseph Richardson. All rights reserved.
+//
+
+import Foundation
+
+
+func getJoke() -> String {
+    return "this app is a joke"
+}
+
+
+func getRiddle() -> String {
+    return "this app is becoming a riddle"
+}
